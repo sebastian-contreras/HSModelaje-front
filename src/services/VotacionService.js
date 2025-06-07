db826a01106b911445fa63f890559213e9a024d6
Sebastian Contreras <sebasfac2017@gmail.com>
2025-06-07 18:59:12 -0300

    feat: Add API endpoints for voting management

--- src/services/VotacionService.js
+++ src/services/VotacionService.js
@@ -1,9 +1,42 @@
 import axios from '../config/AxiosConfig.js'
 import { API_URL } from '../Fixes/API_URL.js'
 
-export async function InicioVotacionModeloApi (IdModelo) {
-  const response = await axios.get(`${API_URL}/api/votacion/modelo/` + IdModelo)
-  console.log(response)
+export async function InicioVotacionModeloApi (IdParticipante) {
+  const response = await axios.get(`${API_URL}/api/votos/iniciar-voto/` ,{
+    params: {
+      pIdParticipante: IdParticipante
+    }
+  })
+  return response.data
+}
+export async function DetenerVotacionModeloApi (IdParticipante) {
+  const response = await axios.get(`${API_URL}/api/votos/detener-voto/` ,{
+    params: {
+      pIdParticipante: IdParticipante
+    }
+  })
   return response.data
 }
 
+export async function listarVotacionApi (IdEvento) {
+  const response = await axios.get(`${API_URL}/api/votos`, {
+    params: {
+      pIdEvento: IdEvento
+    }
+  })
+  return response.data
+}
+
+export async function enviarVotacionJuezApi (data) {
+  const response = await axios.post(`${API_URL}/api/votos`,data)
+  return response.data
+}
+
+export async function reiniciarVotacionApi (data) {
+  const response = await axios.get(`${API_URL}/api/votos/reiniciar-voto`,{
+    params: {
+      pIdParticipante: data
+    }
+  })
+  return response.data
+}

bc1f16cbf7756adf51a7d48413057d86439860d9
Sebastian Contreras <sebasfac2017@gmail.com>
2025-06-15 14:56:34 -0300

    feat: Add downloadInformeVotacion function to download voting report

--- src/services/InformesService.js
+++ src/services/InformesService.js
@@ -34,3 +34,33 @@ export async function dashboardApi (id) {
   const response = await axios.get(`${API_URL}/api/dashboard/` + id)
   return response.data
 }
+
+export const downloadInformeVotacion = async (idEvento) => {
+  try {
+    // const response = await axios.get(`${API_URL}/api/informes/${idEvento}`)
+    const response = await axios.post(`${API_URL}/api/informe-votacion/`+idEvento)
+
+    const pdf = response.data.data.pdf
+    const link = document.createElement('a')
+    link.href = `data:application/pdf;base64,${pdf}`
+    link.download = response.data.data.filename // Usa el nombre de archivo recibido
+    link.click()
+
+    const blob = new Blob(
+      [
+        new Uint8Array(
+          atob(pdf)
+            .split('')
+            .map(c => c.charCodeAt(0))
+        )
+      ],
+      { type: 'application/pdf' }
+    )
+    const urlblob = URL.createObjectURL(blob)
+    window.open(urlblob, '_blank') // Abre el PDF en una nueva pestaña
+
+    window.URL.revokeObjectURL(urlblob)
+  } catch (error) {
+    console.error('Error al descargar el PDF:', error)
+  }
+}
